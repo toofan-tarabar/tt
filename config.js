bf94416ef4069e0cb04e98fbddc77564d1e24ba3
toofan-tarabar <zaniar.x2@gmail.com>
2026-08-30 09:24:31 +0330

    Add files via upload

--- config.js
+++ config.js
@@ -2,9 +2,9 @@
 // اگر لینک‌های شیت عوض شد، فقط همین‌ها رو آپدیت کن.
 
 const SHEET_URLS = {
-  iranian: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQUlqtUk2C3n0QaGtzq1LCx1NNVSnZD9kLW9NG2ShzGmUBHuVh4jubRMr-te2q7HUvEWJCfEVicOC6F/pub?gid=0&single=true&output=csv",
-  iraqi:   "https://docs.google.com/spreadsheets/d/e/2PACX-1vQUlqtUk2C3n0QaGtzq1LCx1NNVSnZD9kLW9NG2ShzGmUBHuVh4jubRMr-te2q7HUvEWJCfEVicOC6F/pub?gid=1705380492&single=true&output=csv",
-  announcements: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQUlqtUk2C3n0QaGtzq1LCx1NNVSnZD9kLW9NG2ShzGmUBHuVh4jubRMr-te2q7HUvEWJCfEVicOC6F/pub?gid=930681156&single=true&output=csv"
+  iranian: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQUlqtUk2C3n0QaGtzq1LCx1NNVSnZD9kLW9NG2ShzGmUBHuVh4jubRMr-te2q7HUvEWJCfEVic0C6F/pub?gid=0&single=true&output=csv",
+  iraqi:   "https://docs.google.com/spreadsheets/d/e/2PACX-1vQUlqtUk2C3n0QaGtzq1LCx1NNVSnZD9kLW9NG2ShzGmUBHuVh4jubRMr-te2q7HUvEWJCfEVic0C6F/pub?gid=1705380492&single=true&output=csv",
+  announcements: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQUlqtUk2C3n0QaGtzq1LCx1NNVSnZD9kLW9NG2ShzGmUBHuVh4jubRMr-te2q7HUvEWJCfEVic0C6F/pub?gid=930681156&single=true&output=csv"
 };
 
 // هر چند ثانیه یک‌بار خودکار داده‌ها رو تازه کنه (۶۰ ثانیه)
